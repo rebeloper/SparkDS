8f5ce73356f5c0b82282fae93ae8d8700260c9a5
Alex Nagy <therebeloper@gmail.com>
2020-02-04 18:08:44 +0200

    Linked List adopts Collection

--- Sources/SparkDS/LinkedList.swift
+++ Sources/SparkDS/LinkedList.swift
@@ -94,6 +94,47 @@ public struct LinkedList<Value> {
     }
 }
 
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        public var node: Node<Value>?
+        
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+        
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node, next: { $0?.next })
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+    
+    public var startIndex: Index {
+        Index(node: head)
+    }
+    
+    public var endIndex: Index {
+        Index(node: tail?.next)
+    }
+    
+    public func index(after i: Index) -> Index {
+        Index(node: i.node?.next)
+    }
+    
+    public subscript(position: Index) -> Value {
+        position.node!.value
+    }
+}
+
 extension LinkedList: CustomStringConvertible {
     public var description: String {
         guard let head = head else { return "Empty linked list" }
